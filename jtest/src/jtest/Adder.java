d0c26e5be2dbfc3a7fd9a94d35bc207fa5c0a119
rmiah04 <rmiah04@dcs.bbk.ac.uk>
2014-10-14 20:46:54 +0100

    junit

---
+++ jtest/src/jtest/Adder.java
@@ -0,0 +1,17 @@
+package jtest;
+
+public class Adder {
+
+	public int add(int input1, int input2) {
+		// TODO Auto-generated method stub
+		int ret = input1 + input2;
+		return ret;
+	}
+
+	public int sub(int input1, int input2) {
+		// TODO Auto-generated method stub
+		int ret = input1 - input2;
+		return ret;
+	}
+
+}
